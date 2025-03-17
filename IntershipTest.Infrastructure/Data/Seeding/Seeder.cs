bc7bb4febf3a66f83cd25f15cffe33a3cad737a1
Quinten Aspeslagh <quinten.aspeslagh@student.howest.be>
2025-03-17 12:27:30 +0100

    Add seeding

---
+++ IntershipTest.Infrastructure/Data/Seeding/Seeder.cs
@@ -0,0 +1,29 @@
+﻿using IntershipTest.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntershipTest.Infrastructure.Data.Seeding
+{
+    public static class Seeder
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+               new Team { Id = 1, Name = "Team A" },
+               new Team { Id = 2, Name = "Team B" },
+               new Team { Id = 3, Name = "Team C" }
+            );
+            modelBuilder.Entity<Player>().HasData(
+                new Player { Id = 1, FirstName = "John", LastName = "Doe", DateOfBirth = new DateTime(1995, 5, 15), TeamId = 1 },
+                new Player { Id = 2, FirstName = "Jane", LastName = "Smith", DateOfBirth = new DateTime(1996, 8, 25), TeamId = 1 },
+                new Player { Id = 3, FirstName = "Mike", LastName = "Johnson", DateOfBirth = new DateTime(1994, 2, 10), TeamId = 2 },
+                new Player { Id = 4, FirstName = "Emily", LastName = "Davis", DateOfBirth = new DateTime(1993, 12, 5), TeamId = 2 },
+                new Player { Id = 5, FirstName = "James", LastName = "Williams", DateOfBirth = new DateTime(1997, 7, 21), TeamId = 3 }
+            );
+        }
+    }
+}
---
+++ IntershipTest.Infrastructure/Migrations/20250317112646_seeding.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IntershipTest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Team A" },
+                    { 2, "Team B" },
+                    { 3, "Team C" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "Id", "DateOfBirth", "FirstName", "LastName", "TeamId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1995, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "John", "Doe", 1 },
+                    { 2, new DateTime(1996, 8, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Jane", "Smith", 1 },
+                    { 3, new DateTime(1994, 2, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Mike", "Johnson", 2 },
+                    { 4, new DateTime(1993, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "Emily", "Davis", 2 },
+                    { 5, new DateTime(1997, 7, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "James", "Williams", 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
