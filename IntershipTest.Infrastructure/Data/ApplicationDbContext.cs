eae6aa6e49a7b5a153c9a7b869a5f987ffb842d3
Quinten Aspeslagh <quinten.aspeslagh@student.howest.be>
2025-03-17 12:23:38 +0100

    Add application db context

---
+++ IntershipTest.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IntershipTest.Core.Entities;
+using IntershipTest.Infrastructure.Data.Seeding;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntershipTest.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Seeder.Seed(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
